606075a5a1685d0623768a40ee4ffda6dc7ecdcb
CampAndy1995 <144943158+CampAndy1995@users.noreply.github.com>
2023-12-10 09:23:47 -0800

    first commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+alert('ONLY YOU')     // This is showing what phrase the alert will populate. //
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate").innerHTML=sentence;
+}
